7eccc9f80790453b840788a351b95102a0639d2f
Murray Chen <mchen92@illinois.edu>
2016-09-08 13:56:41 +0800

    set up basic skeleton of code

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.addressbook.data.person.Name;
+
+public class NameTest {
+	
+}
