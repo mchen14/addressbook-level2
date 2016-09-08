dbe3171eeb21dc9b1b599c8e8537211a1a274628
Murray Chen <mchen92@illinois.edu>
2016-09-08 14:02:33 +0800

    added null test

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -2,12 +2,27 @@ package seedu.addressbook.data.person;
 
 import java.util.Arrays;
 import java.util.List;
+
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Name;
 
 public class NameTest {
+	private Name name;
+	@Before
+	public void setup() throws IllegalValueException{
+		name = new Name("Charlie");
+	}
+	
+	@Test
+	public void checkIsSimilarNull(){
+		Name other = null;
+		assertEquals(name.isSimilar(other), false);
+	}
+	
 	
 }
